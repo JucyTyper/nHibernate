740687352f4e759f9ca1e8b3d80cc7aef089b6ff
ajay-chicmic <ajay.joshi@chicmic.co.in>
2023-04-06 11:58:04 +0530

    Add project files.

---
+++ nHibernate/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿
+namespace nHibernate.Models
+{
+    public class Product
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Role { get; set; }
+    }
+}
---
+++ nHibernate/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using nHibernate.Models;
+using NHibernate;
+
+namespace nHibernate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string connStr = "Data Source=112.196.113.3,1535;Initial Catalog=Test;User ID=sa;Password=Mssql@chicmic2023;Trusted_Connection = True;TrustServerCertificate=True; Integrated Security=false";
+            var config = new NHibernate.Cfg.Configuration();
+            /*config.DataBaseIntegration(d=>{
+                d.ConnectionString = connStr;
+                d.Dialect<MsSql2012Dialect>();
+                d.Driver<SqlClientDriver>();
+            });*/
+            config.Configure("C:/Users/ChicMic/source/repos/nHibernate/nHibernate/XMLFile1.cfg.xml");
+            config.AddAssembly(typeof(Product).Assembly);
+            ISessionFactory _sessionFactory = config.BuildSessionFactory();
+
+            using (var session = _sessionFactory.OpenSession())
+            {
+                var product = new Product { Id= 1 ,Name = "Ajay",Role ="Intern"};
+                session.Save(product);
+            }
+        }
+    }
+}
